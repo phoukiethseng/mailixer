7d1e34891a1ab3d0469813b90e3febc28b858445
Phou Kiethseng <puseng123@gmail.com>
2023-09-26 04:58:47 +0800

    Optimized imports

--- resources/js/Components/SubscriberTable/Columns.tsx
+++ resources/js/Components/SubscriberTable/Columns.tsx
@@ -4,8 +4,6 @@ import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "../DropDownMenu";
 import { Button } from "../Button";
